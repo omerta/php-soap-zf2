97aeabcacb8486d69e8b8a8d88752778ca2013ce
Guillermo Lengemann <glengemann@opsu.gob.ve>
2014-09-10 16:14:15 -0430

    publicar un array asociado

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Primeros paso para publicar servicios web con php usando soap y zend framework
 * PHP5
 * SOAP
 * Zend Framework 2
+
+## Bibliografía ##
+
+* http://framework.zend.com/manual/2.0/en/modules/zend.soap.auto-discovery.html
+* http://www.king-foo.be/2011/09/using-complex-types-with-zend_soap/
++ SOAP and PHP in 2014. Fuente: http://www.whitewashing.de/2014/01/31/soap_and_php_in_2014.html
++ https://github.com/yjwei/practices/tree/802ab1299f9c79cd67fc85bb40345a18fe0460f2/webservice
---
+++ io/arrayAssoComplexType/server.php
@@ -0,0 +1,51 @@
+<?php
+ini_set("soap.wsdl_cache_enabled", "0");
+
+class Universidad {
+	/** @var int */
+	public $cod_institucion;
+	/** @var string */
+	public $siglas;
+	/** @var int */
+	public $cod_carrera;
+}
+
+class educacion{
+	/**
+	 * @return Universidad[] arrayOfLetters
+	 */
+	public function universidad()
+	{
+		$arreglo[]=array("cod_institucion"=>'000001',"siglas"=>'UCV',"cod_carrera"=>'006');
+		$arreglo[]=array("cod_institucion"=>'000002',"siglas"=>'UCV',"cod_carrera"=>'007');
+		$arreglo[]=array("cod_institucion"=>'000003',"siglas"=>'UNEFA',"cod_carrera"=>'008');
+		//print_r($arreglo); // descomentar junto a: debug1
+		return $arreglo;
+	}
+}
+
+// $return_philosopher=new educacion(); // descomentar junto a: debug1
+// $return_philosopher->universidad(); // descomentar junto a: debug1
+
+/* load framework ZF2 */
+set_include_path('/srv/www/io/ZendFramework-2.3.2/library');
+require_once 'Zend/Loader/StandardAutoloader.php';
+$loader = new Zend\Loader\StandardAutoloader(array('autoregister_zf' => true));
+$loader->registerPrefix('Zend', '/srv/www/io/ZendFramework-2.3.2/library');
+$loader->register();
+
+/* class autodiscover and Zend\Soap\Server */
+if (isset ( $_GET ['wsdl' ] )) {
+	$strategy = new Zend\Soap\Wsdl\ComplexTypeStrategy\ArrayOfTypeComplex ();
+	$server = new Zend\Soap\AutoDiscover ( $strategy );
+	$server->setServiceName ( 'WebServiceEducacion' );
+} else {
+	$server = new Zend\Soap\Server ();
+}
+
+$uri = 'http://glengemann.io/arrayAssoComplexType/server.php';
+$server->setClass ('educacion');
+$server->setUri ( $uri );
+$server->handle ();
+
+?>
